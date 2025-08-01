370d4d9e054da02c5ef11a465f4d03fccad88bd7
LuisoPadilla <estrada449@hotmail.com>
2025-07-31 18:10:50 -0600

    Agregar Create Autor

---
+++ LibreriaEcommerce.Api.Autor/Application/Nuevo.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation;
+using LibreriaEcommerce.Api.Autor.Data.Context;
+using LibreriaEcommerce.Api.Autor.Data.Entities;
+using MediatR;
+
+namespace LibreriaEcommerce.Api.Autor.Application
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest<Unit>
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+            public Guid AutorLibroId { get; set; }
+        }
+
+        public class Validador : AbstractValidator<Ejecuta>
+        {
+            public Validador()
+            {
+                RuleFor(x => x.Nombre).NotEmpty().WithMessage("El campo Nombre es requerido");
+                RuleFor(x => x.Apellido).NotEmpty().WithMessage("El campo Nombre es requerido");
+            }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta, Unit>
+        {
+            private readonly AppDbContext dbContext;
+
+            public Manejador(AppDbContext dbContext)
+            {
+                this.dbContext = dbContext;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var autorEntity = new AutorEntity
+                    {
+                         Nombre = request.Nombre,
+                         Apellido = request.Apellido,
+                         FechaNacimiento = request.FechaNacimiento,
+                         AutorLibroId = Guid.NewGuid()
+                    };
+                    await dbContext.Autores.AddAsync(autorEntity);
+                    var rows = await dbContext.SaveChangesAsync();
+                    if (rows == 0)
+                    {
+                        throw new Exception($"No se pudo agregar el Autor {autorEntity.Nombre} - {autorEntity.Apellido}");
+                    }
+                    return Unit.Value;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
---
+++ LibreriaEcommerce.Api.Autor/Data/Entities/Autor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibreriaEcommerce.Api.Autor.Data.Entities
+{
+    public class AutorEntity
+    {
+        [Key]
+        public int AutorId { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "El campo {0} es debe de contenter entre {2} y {1} caracteres", MinimumLength = 2)]
+        public required string Nombre { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "El campo {0} es debe de contenter entre {2} y {1} caracteres", MinimumLength = 2)]
+        public required string Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public Guid AutorLibroId { get; set; }
+    }
+}
